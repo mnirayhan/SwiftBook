f0022438d1a560c2386b0ef1b3251ee46b0798e4
rayhan <rayhanstack@gmail.com>
2025-07-26 13:41:27 +0600

    refector: updated build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.ycngmn.nobook"
+        applicationId = "com.rayhan.metapipe"
         minSdk = 22
         targetSdk = 35
         versionCode = 10
